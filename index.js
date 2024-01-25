14ab7ac28bfb06a88253f1c1ce96b57cc464bd52
7oda-mo <130270152+7oda-mo@users.noreply.github.com>
2024-01-25 19:51:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({ checkUpdate: false });
+
+client.on('ready', async () => {
+  console.log(`${client.user.username} is Ready For Working 24/7!`);
+  client.user.setActivity('صلي علي الحبيب المصطفي ✨️👑', {
+    type: 'STREAMING',
+    url: 'https://twitch.tv/mogg' 
+  });
+});
+
+client.login(process.env.token);
